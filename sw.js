8a1c31d3bd2fc59a8b98557b12063aa380974898
Davidliams12 <bugattillon8@gmail.com>
2026-04-25 19:35:51 +0100

    Implement service worker for caching assets
    Added service worker caching for offline support.

--- sw.js
+++ sw.js
@@ -1,4 +1,24 @@
-self.addEventListener('fetch', (event) => {
-    // This allows the app to work offline once cached
-    event.respondWith(fetch(event.request));
+const CACHE_NAME = 'virtue-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/Ikon.jpg'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Listen for requests (Required for Install Button)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
